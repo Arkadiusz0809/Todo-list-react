d4ac5161211b4f947eb9415306fae90df537b64c
Arkadiusz Torun <arek.torun12@gmail.com>
2024-02-17 20:53:32 +0100

    replaced and renamed to button_buttons to Buttons component

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -3,10 +3,10 @@ import "./style.css"
 const Buttons = ({ tasks, hideDoneTasks }) => (
     tasks.length > 0 && (
         <div className="buttons">
-            <button className="task__buttonHide">
+            <button className="buttons_button">
                 {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
             </button>
-            <button className="task__buttonComplete"
+            <button className="buttons_button"
                 disabled={tasks.every(({ done }) => done)}
             >
                 Ukończ wszystkie
