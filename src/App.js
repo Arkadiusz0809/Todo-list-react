0f765c94f6e5ab64dfdf877920e03a48d8867a65
Arkadiusz Torun <arek.torun12@gmail.com>
2024-02-18 16:56:56 +0100

    hideDoneTasks renamed to hideDone

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import Buttons from "./Buttons";
 import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
+import { hello } from "./utils/hello";
 
-
+hello();
 
 const tasks = [
   { id: 1, content: "mleko", done: false },
   { id: 2, content: "banan", done: true },
 ];
 
-const hideDoneTasks = false;
+const hideDone = false;
 
 function App() {
   return (
@@ -24,8 +25,8 @@ function App() {
       />
       <Section
         title="Lista zadań"
-        body={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />}
-        extraHeaderContent={<Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />}
+        body={<Tasks tasks={tasks} hideDone={hideDone} />}
+        extraHeaderContent={<Buttons tasks={tasks} hideDone={hideDone} />}
       />
     </Container>
   );
--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,10 +1,10 @@
 import "./style.css"
 
-const Buttons = ({ tasks, hideDoneTasks }) => (
+const Buttons = ({ tasks, hideDone }) => (
     tasks.length > 0 && (
         <div className="buttons">
             <button className="buttons_button">
-                {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
+                {hideDone ? "Pokaż" : "Ukryj"} ukończone
             </button>
             <button className="buttons_button"
                 disabled={tasks.every(({ done }) => done)}
