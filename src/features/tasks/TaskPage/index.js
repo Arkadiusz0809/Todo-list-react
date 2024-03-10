5c37a5b6a9068d78215136282de4d2d11101d5e2
Arkadiusz Torun <arek.torun12@gmail.com>
2024-03-10 19:21:21 +0100

    error handling

--- src/features/tasks/TaskPage/index.js
+++ src/features/tasks/TaskPage/index.js
@@ -14,11 +14,11 @@ function TaskPage() {
             <Header title="Szczegóły zadania" />
             <Section
                 title={task ? task.content : "Nie znaleziono zadania 🙃"}
-                body={
+                body={!!task && (
                     <>
                         <strong>Ukończono:</strong> {task.done ? "Tak" : "Nie"}
                     </>
-                }
+                )}
             />
         </Container>
     );
