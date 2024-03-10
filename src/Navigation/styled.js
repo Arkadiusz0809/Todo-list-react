ddbc99b662db61b0871e91c08b7ac6a63161b76b
Arkadiusz Torun <arek.torun12@gmail.com>
2024-03-10 19:15:39 +0100

    added navigation component

---
+++ src/Navigation/styled.js
@@ -0,0 +1,30 @@
+import { NavLink } from "react-router-dom/cjs/react-router-dom";
+import styled from "styled-components";
+
+
+export const StyledNavLink = styled(NavLink)`
+    color: white;
+    text-decoration: none;
+
+    &.active {
+        font-weight: bold;
+    }
+
+    &:hover{
+        border-bottom: 1px solid;
+    }
+`;
+
+export const List = styled.ul`
+    background: teal;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    list-style: none;
+`;
+
+export const Item = styled.li`
+    margin: 20px;
+`;
+
